bbaed62ba1c7e99fcbeaf372d29288aadd31ba6f
harmanjot78 <91528764+harmanjot78@users.noreply.github.com>
2021-10-06 15:54:52 +0530

    Update options.ts

--- src/options.ts
+++ src/options.ts
@@ -3,14 +3,17 @@ import * as core from '@actions/core'
 export interface Options {
   project: string
   ignoreAuthor: string[]
+  issue: string
 }
 
 export function getInput(): Options {
   const project = core.getInput('project', {required: true})
   const ignoreAuthor = core.getInput('ignore-author').split(',')
+  const issue = core.getInput('issue')
 
   return {
     project,
+    issue,
     ignoreAuthor
   }
 }
